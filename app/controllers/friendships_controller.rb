0a5b6936536ec167c318f84273b79f756274d7f3
RhythmAg <ragrawal@isystango.com>
2020-03-12 20:08:03 +0530

    Fixes

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,13 +4,13 @@ class FriendshipsController < ApplicationController
 	def create
 		current_user.follow(@user)
 		redirect_to profile_path(@user)
-		flash[:success] = 'You followed the user'
+		flash[:success] = 'You followed the user.'
 	end
 
 	def destroy
 		current_user.unfollow(@user)
 		redirect_to profile_path(@user)
-		flash[:success] = 'The user was successfully unfollow'
+		flash[:success] = 'The user was successfully unfollowed.'
 	end
 
 	private
