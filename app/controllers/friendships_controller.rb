09319ef05f64f58bf9bfa4eb5af0b6f82e9664ce
RhythmAg <ragrawal@isystango.com>
2020-03-16 14:45:04 +0530

    Interpolation in flash

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,13 +4,13 @@ class FriendshipsController < ApplicationController
 	def create
 		current_user.follow(@user)
 		redirect_to profile_path(@user)
-		flash[:success] = 'You followed the user.'
+		flash[:success] = "You followed the #{@user.email}."
 	end
 
 	def destroy
 		current_user.unfollow(@user)
 		redirect_to profile_path(@user)
-		flash[:success] = 'The user was successfully unfollowed.'
+		flash[:success] = "The #{@user.email} was successfully unfollowed."
 	end
 
 	private
