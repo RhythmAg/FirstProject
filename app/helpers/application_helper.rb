ce3f9b0ea3b4344560efa49c85cf59929bffcca0
RhythmAg <ragrawal@isystango.com>
2020-03-17 13:12:20 +0530

    Notification in desc order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
 		@obj = params[:controller] == "news" ? News.find(params[:id]) : Article.find(params[:id])
 	end
 
+  def notification_list
+    @notifications = current_user.notifications.order(created_at: :desc)
+  end
+
 end
